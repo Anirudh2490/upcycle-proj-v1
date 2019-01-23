7ced31e69f341ac84f54807b103c8dfdcc2416b3
AB Rao <anirudhrao24@gmail.com>
2019-01-23 02:48:32 +0100

    changes to Collections.js

--- models/Collections.js
+++ models/Collections.js
@@ -1,10 +1,12 @@
 const mongoose     = require('mongoose');
 
 const CollectionsSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  materialType: {type: String, enum: ['Cotton', 'Wool', 'Denim', 'Linen', 'Jersey', 'Silk', 'Polycotton' , 'Velvet', 'Synthetics', 'Other'], required: true},
+  designer: mongoose.Schema.Types.ObjectId,
+  materialType: {type: String, required: true},
   cost: {type: String, required: true},
-  weight: {type: String, required: true}
+  weight: {type: String, required: true},
+  description: {type: String, required: true}
+  image: {type: String, data: Buffer}
 })
 
 const Materials = mongoose.model('Materials', MaterialsSchema);
