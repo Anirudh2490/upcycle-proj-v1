7c580291e2fc76c970b00694f3c5a2f61d11204e
AB Rao <anirudhrao24@gmail.com>
2019-02-07 21:11:51 +0100

    Fix typo

--- models/Materials.js
+++ models/Materials.js
@@ -4,7 +4,7 @@ const MaterialsSchema = mongoose.Schema({
   //_id: mongoose.Schema.Types.ObjectId,
   fullname: {type: String, required: true},
   email: {type: String, required: true},
-  phone: type: Number,
+  phone: {type: Number},
   sellingChoice: {type:String, enum: ['Sell my clothes', 'Upcycle clothes into something cool', 'Give the money now, get paid later']},
   message: {type:String},
 })
