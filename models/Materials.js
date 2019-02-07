d6e8fb3b6c0063592314e9b55708b3ed1dbc2080
AB Rao <anirudhrao24@gmail.com>
2019-02-07 20:55:54 +0100

    Material.js

--- models/Materials.js
+++ models/Materials.js
@@ -2,9 +2,11 @@ const mongoose     = require('mongoose');
 
 const MaterialsSchema = mongoose.Schema({
   //_id: mongoose.Schema.Types.ObjectId,
-  materialType: {type: String, enum: ['Cotton', 'Wool', 'Denim', 'Linen', 'Jersey', 'Silk', 'Polycotton' , 'Velvet', 'Synthetics', 'Other'], required: true},
-  cost: {type: String, required: true},
-  weight: {type: String, required: true}
+  fullname: {type: String, required: true},
+  email: {type: String, required: true},
+  phone: type: Number,
+  sellingChoice: {type:String, enum: ['Sell my clothes', 'Upcycle clothes into something cool', 'Give the money now, get paid later']},
+  message: {type:String},
 })
 
 const Materials = mongoose.model('Materials', MaterialsSchema);
